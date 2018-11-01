092a3a96150a0528a4d349ec654bcdd63fa8dcdd
mizokami <r.mizokami@gmail.com>
2018-11-01 11:07:12 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,70 @@
+fn compile(input: &str) -> String {
+    let mut code = input;
 
-struct Compiler {
-    program: String,
+    let mut code = & mut code;
+
+    eval(& mut code)
 }
 
-impl Compiler {
-    fn new(program: &str) -> Self {
-        Self {
-            program: program.to_string(),
-        }
-    }
+fn eval(code: & mut &str) -> String {
+    // FIXME
+    let mut lines = String::new();
 
-    fn compile(&self) -> &str {
-        // FIXME
-        let chars = self.program.chars();
+    let chars = code.chars();
 
-        for c in chars {
-            match c {
-                ' ' => {
-                    // do nothing
-                },
-                '+' | '-' | '*' | '/' => {
-                    println!("{}", c);
-                },
-                '0'...'9' => {
-                    println!("{}", c);
-                },
-                x => {
-                    panic!("Invalid token: {:?}", x);
-                },
+    let mut pos: usize = 0;
+
+    let first_char = chars.next().unwrap();
+
+    pos += 1;
+
+    let mut num = 0;
+
+    if first_char.is_digit(10) {
+        num = first_char.to_digit(10).unwrap();
+
+        for (i, c) in chars.enumerate() {
+            if let Some(digit) = c.to_digit(10) {
+                pos += 1;
+                num = digit + num * 10;
+            } else {
+                lines.push_str(&format!("mov r0, {}\n", num));
+                lines.push_str("push r0");
             }
         }
-
-        r"push r0
-mov r0, 1
-add r0, 2
-pop r0"
     }
+
+    // for c in chars {
+    //     match c {
+    //         ' ' => {
+    //             // do nothing
+    //         },
+    //         '+' | '-' | '*' | '/' => {
+    //             println!("{}", c);
+    //         },
+    //         '0'...'9' => {
+    //             lines.push("mov r0, {}");
+    //             lines.push("push r0");
+    //         },
+    //         x => {
+    //             panic!("Invalid token: {:?}", x);
+    //         },
+    //     }
+    // }
+
+    lines
 }
 
 fn main() {
-    let compiler = Compiler::new("1 2 +");
-
-    compiler.compile();
+    // FIXME
 }
 
 #[test]
-fn test_compiler_compile_add() {
-    let compiler = Compiler::new("1 2 +");
+fn test_compiler_compile() {
+    {
+        let compiler = Compiler::new("1".to_string());
 
-    assert_eq!(compiler.compile(), r"push r0
-mov r0, 1
-add r0, 2
-pop r0");
-}
-
-#[test]
-fn test_compiler_compile_function() {
-    let compiler = Compiler {
-        program: "A[a + b] A(1 2)".to_string(),
-    };
-
-    assert_eq!(compiler.compile(), r"mov r1, 1
-mov r2, 2
-call A
-A:
-push r1
-push r2
-add r1, r2
-move r0, r1
-pop r2
-pop r1
-ret");
+        assert_eq!(compiler.compile(), r"mov r0, 1
+push r0");
+    }
 }
