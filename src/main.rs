4db6e9718a837b44e3465c0603df36fe8847a968
nakaly <ryosuke.nakamura@gmail.com>
2018-11-01 12:33:32 +0900

    Support add

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
+use std::env;
+
 fn compile(input: &str) -> String {
+
+    let mut assembly = String::new();
+    assembly.push_str(r".ORG     $0000	// IPLエリア
+         call	_MAIN
+         halt
+
+.ORG     $1000	// ユーザエリア
+_MAIN:
+");
+    assembly.push_str(&eval(input));
+    assembly.push_str("    ret\n");
+    assembly
+}
+fn eval(input: &str) -> String {
     let mut code = input;
-
     let mut code = & mut code;
-
-    eval(& mut code)
+    eval_r(&mut code)
 }
 
-fn eval(code: & mut &str) -> String {
+fn eval_r(code: & mut &str) -> String {
+    if code.len() < 1 {
+        return String::new();
+    }
     // FIXME
     let mut lines = String::new();
 
-    let chars = code.chars();
+    let mut chars = code.chars();
 
     let mut pos: usize = 0;
 
@@ -28,10 +45,31 @@ fn eval(code: & mut &str) -> String {
                 pos += 1;
                 num = digit + num * 10;
             } else {
-                lines.push_str(&format!("mov r0, {}\n", num));
-                lines.push_str("push r0");
+                break;
+            }            
+        }
+        lines.push_str(&format!("    mov r0, {}\n", num));
+        lines.push_str("    push r0\n");
+        *code = &code[pos..];
+        lines.push_str(&eval_r(code))
+    } else {
+        match first_char {
+            ' ' => {
+                //do nothing
+            }
+            '+' => {
+                lines.push_str(r"    pop r1
+    pop r0
+    add r0, r1
+    push r0
+");
+            },
+            x => {
+               panic!("Invalid token: {:?}", x); 
             }
         }
+        *code = &code[1..];
+        lines.push_str(&eval_r(code))
     }
 
     // for c in chars {
@@ -56,15 +94,30 @@ fn eval(code: & mut &str) -> String {
 }
 
 fn main() {
-    // FIXME
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        panic!("at least one arg is required")
+    }
+    println!("{}",compile(args.get(1).unwrap()));
 }
 
 #[test]
 fn test_compiler_compile() {
     {
-        let compiler = Compiler::new("1".to_string());
-
-        assert_eq!(compiler.compile(), r"mov r0, 1
-push r0");
+        assert_eq!(eval("1"), r"    mov r0, 1
+    push r0
+");
+        assert_eq!(eval("12"), r"    mov r0, 12
+    push r0
+");
+        assert_eq!(eval("1 2 +"), r"    mov r0, 1
+    push r0
+    mov r0, 2
+    push r0
+    pop r1
+    pop r0
+    add r0, r1
+    push r0
+");
     }
 }
